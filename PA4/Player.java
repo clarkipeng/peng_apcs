ce123e809a0bf6703c6d8247e6143e5abbb48173
Clark Peng <clarkpeng@Clarks-MacBook-Air.local>
2021-09-10 13:41:51 -0700

    PA4 working version

---
+++ PA4/Player.java
@@ -0,0 +1,174 @@
+import java.io.*;
+import java.util.*;
+
+public class Player{
+    String name;
+    double hp;
+    int pd=0,md=0;//hp = health, pd = physical defense, md = magic defense;
+
+    public Player(String name){
+        this.name = name;
+        hp = 10;
+        pd = 0;
+        md = 0;
+    }
+    public void printStats(){
+        System.out.println("HP: "+ hp+"| Physical Defense: "+pd+" | Magical Defense: "+md);
+    }
+    public void promptStats(Scanner Input){
+        // Scanner s = new Scanner(System.in);
+        int points = 20;
+        while(points>0){
+            int i = 21;
+            while(i >points){
+                System.out.println("How many points do you want in physical defense? "
+                                    + points+" points left");
+                i = Input.nextInt();
+                if(i>points)System.out.println("Not enough points");
+            }
+            pd += i;
+            points-=i;
+            i=21;
+            while(i >points){
+                System.out.println("How many points do you want in magic defense? "
+                                    + points+" points left");
+                i = Input.nextInt();
+                if(i>points)System.out.println("Not enough points");
+            }
+            md += i;
+            points-=i;
+            if(points>0)System.out.println("You still have points left");
+        }
+        // s.close();
+    }
+    public void doAction(int pa, Player p1,int p1a){
+        //a =action = 0 = physical, 1 = spell, 2 = defense, 3 = heal.
+        switch(pa){
+            case 0:{
+                switch(p1a){
+                    case 0:break;
+                    case 1:{
+                        getAttacked(p1,this,pa);
+                        break;
+                    }
+                    case 2:{
+                        Attackrepelled(p1,this,pa);
+                        break;
+                    }
+                    case 3:{
+                        heal(p1);
+                        getAttacked(p1,this,pa);
+                        break;
+                    }
+                }
+                break;
+            }
+            case 1:{
+                switch(p1a){
+                    case 0:{
+                        getAttacked(this,p1,p1a);
+                        break;
+                    }
+                    case 1:break;
+                    case 2:{
+                        getAttacked(p1,this,pa);
+                        break;
+                    }
+                    case 3:{
+                        heal(p1);
+                        getAttacked(p1,this,pa);
+                        break;
+                    }
+                }
+                break;
+            }
+            case 2:{
+                switch(p1a){
+                    case 0:{
+                        Attackrepelled(this,p1,p1a);
+                        break;
+                    }
+                    case 1:{
+                        getAttacked(this,p1,p1a);
+                        break;
+                    }
+                    case 2:break;
+                    case 3:{
+                        heal(p1);
+                        break;
+                    }
+                }
+                break;
+            }
+            case 3:{
+                heal(this);
+                switch(p1a){
+                    case 0:{
+                        getAttacked(this,p1,p1a);
+                        break;
+                    }
+                    case 1:{
+                        getAttacked(this,p1,p1a);
+                        break;
+                    }
+                    case 2:{
+                        break;
+                    }
+                    case 3:{
+                        heal(p1);
+                        break;
+                    }
+                }
+                break;
+            }
+        }
+    }
+    public void Attackrepelled(Player defender,Player attacker,int attacktype){
+        if(attacktype==0){
+            attacker.hp-=.5;
+            System.out.println(attacker.name+"'s attack was reflected!");
+        }
+    }
+    public void getAttacked(Player defender,Player attacker,int attacktype){
+        if(attacktype==0){
+            if(rollattack()>=defender.pd){
+                defender.subHealth(1);
+                System.out.println(attacker.name+"'s attack worked!");
+            }
+            else System.out.println(attacker.name+"'s attack failed!");
+        }
+        else if(attacktype ==1){
+            if(rollattack()>=defender.md){
+                defender.subHealth(1);
+                System.out.println(attacker.name+"'s attack worked!");
+            }
+            else System.out.println(attacker.name+"'s attack failed!");
+        }
+    }
+    public int rollattack(){
+        return (int)(Math.random()*31);
+    }
+    public void heal(Player a){
+        a.addHealth(0.5);
+        System.out.println(a.name+" Healed!");
+    }
+    public void generateStats(int param){//param is max points
+        pd = param/2+(int)(Math.random()*param/2);
+        md =param-pd;
+    }
+    public int generateMove(){
+        if(hp==10){
+            return (int)(Math.random()*3);
+        }
+        else{
+            return (int)(Math.random()*4);
+        }
+    }
+    public void addHealth(double delta){
+        hp+=delta;
+        hp = Math.min(10,hp);
+    }
+    public void subHealth(double delta){
+        hp-=delta;
+    }
+}
---
+++ PA4/test.java
@@ -0,0 +1,41 @@
+//package cpeng.peng_PA2;
+import java.util.*;
+import java.io.*;
+
+
+public class test{
+
+    public static void main(String[]args){
+        Scanner Input = new Scanner(System.in);
+        Player p = new Player("Player");
+        Player ai = new Player("Opponent");
+        p.promptStats(Input);
+        ai.generateStats((int)(Math.random()*30)+20);
+        HashMap<Integer,String> moves = new HashMap<Integer,String>();
+        moves.put(0,"attacked");moves.put(1,"used magic");
+        moves.put(2,"defended");moves.put(3,"healed");
+        while(p.hp>0&&ai.hp>0){
+            System.out.println("Your opponent's stats:");
+            ai.printStats();
+            System.out.println("Your stats:");
+            p.printStats();
+            int input=-1;
+            while(input<0||input>3){
+                System.out.println("What action will you take against your opponent?"+ 
+                                " {1: attack,2: spell,3: defend,4:heal}");
+                input = Input.nextInt();
+                input--;
+                if(input<4&&input>=0)break;
+                else System.out.println("Not a valid move");
+            }
+            int aimove = ai.generateMove();
+            System.out.println("you "+moves.get(input));
+            System.out.println("your opponent "+moves.get(aimove));
+            p.doAction(input, ai, aimove);
+            
+        }
+        if(p.hp<=0)System.out.println("YOU LOSE!");
+        else System.out.println("YOU WIN!");
+        Input.close();
+    }
+}
