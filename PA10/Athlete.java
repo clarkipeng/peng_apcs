7c221c025889b4b12163f6e1340a5cb374361840
Clark Peng <clarkpeng@Clarks-MacBook-Air.local>
2022-03-28 10:23:34 -0700

    PA10 edit

--- PA10/Athlete.java
+++ PA10/Athlete.java
@@ -1,18 +1,19 @@
 import java.util.ArrayList;
 
-public class Althlete extends Person {
-    static ArrayList<Althlete> althletes=new  ArrayList<Althlete>();
+public class Athlete extends Person {
+    public static ArrayList<Athlete> athletes=new  ArrayList<Athlete>();
     String favoriteEvent;
     float pbRun,pbSwim,pbBike;
+    int exRun,exSwim,exBike;
     int number;
-    public Althlete(String fn,String ln,String fe,float r,float s, float b){
+    public Athlete(String fn,String ln,String fe,float r,float s, float b,int 
+                    er,int es,int eb){
         super(fn,ln);
         favoriteEvent=fe;
-        pbRun =r;
-        pbSwim =s;
-        pbBike =b;
-        number=althletes.size();
-        althletes.add(this);
+        pbRun=r;pbSwim =s;pbBike =b;
+        exRun=er;exSwim=es;exBike=eb;
+        number=athletes.size();
+        athletes.add(this);
         super.people.add(this);
     }
     public float overallTime(){
--- PA10/Staff.java
+++ PA10/Staff.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 public class Staff extends Person {
 
-    static ArrayList<Staff> allStaff= new ArrayList<Staff>();
+    public static ArrayList<Staff> allStaff= new ArrayList<Staff>();
     String certificationType, issuer, locationOfWork;
     public Staff(String fn,String ln,String ct,String i,String lw){
         super(fn,ln);
